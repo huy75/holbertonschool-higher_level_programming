d8b0c2d5fae01e07d22ea24048086d8a38310334
huy75 <huy.nguyen66@gmail.com>
2020-12-08 12:50:44 +0000

    feat uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def islower(c):
+    return ord(c) in range(97, 123)
+
+
+def uppercase(str):
+    for i in str:
+        print("{:c}".format(ord(i) if not islower(i) else ord(i) - 32), end="")
+    print("")

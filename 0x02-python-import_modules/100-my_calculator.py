f9403b52c10ea4be1e238d5d2d77c33e09e92bb1
huy75 <huy.nguyen66@gmail.com>
2020-12-10 16:07:11 +0000

    fix move the imoort statements after the main

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
     op = {"+": add, "-": sub, "*": mul, "/": div}
 
     if len(sys.argv[1:]) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
     inOp = sys.argv[2]
 
     if inOp not in op:
         print("Unknown operator. Only: +, -, * and / available")
-        sys.exit(1)
+        exit(1)
 
     try:
         a = int(sys.argv[1])

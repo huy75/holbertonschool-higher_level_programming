b000069a1361b46fc60cb8da9ae0a0a2ecb9a079
huy75 <huy.nguyen66@gmail.com>
2020-12-15 13:52:55 +0000

    feat matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    list = []
+    for row in matrix:
+        if row:
+            list.append(" ".join(["{:d}".format(x) for x in row]))
+    print("\n".join(list))

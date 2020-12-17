701bf29c8892c1b9e61245b4e854c5033f771b10
huy75 <huy.nguyen66@gmail.com>
2020-12-17 17:33:01 +0000

    feat roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
+def roman_to_int(roman_string):
+    res = 0
+
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+
+    rm = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    for idx in range(len(roman_string)):
+        if idx == len(roman_string) - 1:
+            res += rm[roman_string[idx]]
+        elif rm[roman_string[idx]] >= rm[roman_string[idx + 1]]:
+            res += rm[roman_string[idx]]
+        else:
+            res -= rm[roman_string[idx]]
+
+    return res

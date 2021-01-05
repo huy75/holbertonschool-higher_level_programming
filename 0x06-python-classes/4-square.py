8d0cd2d9dbb9d8bbb6637947567a342fc34acc07
huy75 <huy.nguyen66@gmail.com>
2021-01-05 16:02:22 +0000

    fix pep8

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -38,10 +38,8 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-
     def area(self):
         """
         Returns the area of a square
         """
         return (self.__size * self.__size)
-

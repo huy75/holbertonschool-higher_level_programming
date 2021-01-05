f607f75aa9bc5aafd9ecf1d789b276d197f4e652
huy75 <huy.nguyen66@gmail.com>
2021-01-05 15:59:56 +0000

    feat area and setter / getter

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+This is module 3-square
+It defines a basic class Square
+"""
+
+
+class Square:
+    """
+    This class Square takes one argument.
+
+    Example:
+    x = Square(2)
+
+    """
+    def __init__(self, size=0):
+        """
+        Instantiate a Square object
+        Check if the input value is a positive or null digit
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the area of a square
+        """
+        return (self.__size * self.__size)
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,47 @@
 #!/usr/bin/python3
+"""
+This is module 3-square
+It defines a basic class Square
+"""
+
+
+class Square:
+    """
+    This class Square takes one argument.
+
+    Example:
+    x = Square(2)
+
+    """
+    def __init__(self, size=0):
+        """
+        Instantiate a Square object
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        getter for size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        setter for size
+        Check if the input value is a positive or null digit
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+
+    def area(self):
+        """
+        Returns the area of a square
+        """
+        return (self.__size * self.__size)
+

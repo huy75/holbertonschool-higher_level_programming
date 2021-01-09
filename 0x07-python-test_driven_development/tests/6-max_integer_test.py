ce5c3e7ccff919fd2174f2de9338055631655db4
huy75 <huy.nguyen66@gmail.com>
2021-01-09 10:22:16 +0000

    fix add empty list test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,6 +8,12 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
+    def test_empty_list(self):
+        """
+            Testing with an empty list
+        """
+        self.assertEqual(max_integer([]), None)
+
     def test_right_output(self):
         '''
             Testing when passing and argument that is expected to succeed

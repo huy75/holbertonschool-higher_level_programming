5029030e11096e8da8203fc62778686e05f3b4ac
huy75 <huy.nguyen66@gmail.com>
2021-01-09 09:58:25 +0000

    fix add str format

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -16,7 +16,7 @@ def say_my_name(first_name, last_name=""):
     """
     if first_name is None or not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if last_name is None or not isinstance(last_name, str):
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print("My name is {} {}".format(first_name, last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name))

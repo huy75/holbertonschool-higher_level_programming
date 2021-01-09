8dfb4aa1bc868777fc81f7bed4c2be504419d9a6
huy75 <huy.nguyen66@gmail.com>
2021-01-09 10:02:26 +0000

    fix use type vs isinstance

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -14,9 +14,9 @@ def say_my_name(first_name, last_name=""):
     otherwise raise a TypeError exception with the message
     first_name must be a string or last_name must be a string
     """
-    if first_name is None or not isinstance(first_name, str):
+    if first_name is None or type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
     print("My name is {:s} {:s}".format(first_name, last_name))

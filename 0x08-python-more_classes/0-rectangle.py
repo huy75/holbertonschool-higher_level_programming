0c35a92c4b0ffba9ae4f03bf624b33afe950d11b
huy75 <huy.nguyen66@gmail.com>
2021-01-11 09:19:03 +0000

    first commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is module 0-rectangle
+It defines a basic class Rectangle
+"""
+
+
+class Rectangle:
+    """
+    This class Rectangle is empty now.
+    Example:
+    x = Rectangle()
+    """
+    pass

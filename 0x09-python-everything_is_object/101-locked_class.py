26a94b4412a079519a06969bd3e7f05089d7ec6b
huy75 <huy.nguyen66@gmail.com>
2021-01-12 20:12:37 +0000

    feat the rest

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+Module 101-locked_class.py
+"""
+
+
+class LockedClass:
+    """
+    class Locked Class, no class or object attribute
+    """
+    __slots__ = "first_name"

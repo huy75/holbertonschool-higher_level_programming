d8f952d7519424d771de54ed0237780957d3a6a2
huy75 <huy.nguyen66@gmail.com>
2021-01-19 16:27:31 +0000

    feat only subclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
+# 4-inherits_from.py
+"""
+Defines inherited class-checking function
+"""
+
+
 def inherits_from(obj, a_class):
+    """
+    Checks if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class
+
+    Arguments:
+        obj (anything): The object to check.
+        a_class (type): The class to match the type of obj to.
+
+    Returns:
+        If obj is inherited instance of a_class - True.
+        Otherwise - False.
+    """
+    return isinstance(obj, a_class) and issubclass(type(obj), a_class)

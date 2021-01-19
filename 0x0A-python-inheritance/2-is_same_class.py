56b46795db2363f2f4ca602bc1ed828d14446d6d
huy75 <huy.nguyen66@gmail.com>
2021-01-19 15:07:28 +0000

    feat instance of a class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+# 2-is_same_class.py
+"""
+Defines a class-checking function
+"""
+
+
 def is_same_class(obj, a_class):
+    """
+    Check if the object is exactly an instance of the specified class
+    """
+    return obj.__class__ == a_class

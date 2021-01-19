3e4f160f44171f3e82aeb31d86f15572445ee7dc
huy75 <huy.nguyen66@gmail.com>
2021-01-19 20:49:20 +0000

    feat rebel class

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+# 100-my_int.py
+"""
+Defines a class MyInt that inherits from int.
+"""
+
+
+class MyInt(int):
+    """
+    A rebel: inverts == and !=
+    Inherits from int
+    """
+    def __eq__(self, other):
+        """
+        returns false if two objects are equal
+        """
+        return super(MyInt, self).__ne__(other)
+
+    def __ne__(self, other):
+        """
+        returns true if two objects are not equal
+        """
+        return super(MyInt, self).__eq__(other)

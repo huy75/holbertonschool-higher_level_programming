0ddb65ef118fac7f6b6a9b8756570120a053bcae
huy75 <huy.nguyen66@gmail.com>
2021-01-19 18:23:20 +0000

    feat square1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+# 10-square.py
+"""
+Defines a Rectangle subclass Square.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class square, subclass from Rectangle.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Computes area
+        """
+        return self.__size * self.__size

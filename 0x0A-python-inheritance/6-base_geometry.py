02d3280ba23df99172013623416a96da33bbf819
huy75 <huy.nguyen66@gmail.com>
2021-01-19 16:46:49 +0000

    feat raise exception

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+# 6-base_geometry.py
+"""
+Defines a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    A BaseGepmetry class
+    """
+
+    def area(self):
+        """
+        Not implemented yet
+        """
+        raise Exception("area() is not implemented")

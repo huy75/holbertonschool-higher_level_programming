27c37e09bbb8f014a357aaf7d31257b0b28f42ec
huy75 <huy.nguyen66@gmail.com>
2021-01-20 21:05:58 +0000

    feat pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+# 12-pascal_triangle.py
+"""
+Defines a Pascal's Triangle function.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Represent Pascal's Triangle of size n.
+    Returns a list of lists of integers representing the triangle.
+    """
+    if n < 0:
+        return[]
+
+    triangle = [[1]]
+
+    while (len(triangle) < n):
+        new_line = [1]
+        prev = triangle[-1]
+        for each in range(len(prev) - 1):
+            new_line.append(prev[each] + prev[each + 1])
+        new_line.append(1)
+        triangle.append(new_line)
+
+    return (triangle)

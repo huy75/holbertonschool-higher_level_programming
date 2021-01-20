04872c81ffbfdafa4e7dfd5dcf2141ffcdbc1205
huy75 <huy.nguyen66@gmail.com>
2021-01-20 16:15:28 +0000

    feat dict desc & student 1

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-def append_after(filename="", search_string="", new_string=""):
+# 8-class_to_json.py
+"""
+Defines a Python class-to-JSON function.
+"""
+
+
+def class_to_json(obj):
+    """
+    Return the dictionary represntation of a simple data structure.
+    """
+    return (obj.__dict__)
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+# 9-student.py
+"""
+Defines a Student class."""
+
+
+class Student:
+    """
+    Student class.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a new Student.
+
+        Args:
+            first_name (str): The first name.
+            last_name (str): The last name.
+            age (int): The age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns dictionary description of the instance.
+        """
+        return self.__dict__

64d61cb4df5b7adf7a31ac3308c07143ef1def85
huy75 <huy.nguyen66@gmail.com>
2021-01-25 12:59:10 +0000

    feat add tests from json string

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -115,17 +115,55 @@ were given"
     def test_18_to_json_string_rect(self):
         """Test to_json_string with Rectangle class"""
         r1 = Rectangle(1, 2, 3, 4, 5)
-        r2 = Rectangle(4, 7, 8, 9, 10)
+        r2 = Rectangle(6, 7, 8, 9, 10)
         r3 = Rectangle(12, 12, 13, 14, 15)
 
         dict_r = [r1.to_dictionary(), r2.to_dictionary(), r3.to_dictionary()]
         jd = Base.to_json_string(dict_r)
+        self.assertTrue(type(jd) is str)
         dict_r = str(dict_r)
         dict_r = dict_r.replace("'", '"')
         self.assertEqual(dict_r, jd)
 
+    def test_19_to_json_string_sqr(self):
+        """Test to_json_string with Square class"""
+        s1 = Square(1, 2, 3)
+        s2 = Square(4, 5, 6)
+        s3 = Square(7, 8, 9)
 
-# ---------- task 15 -------------------------------------------
+        dict_s = [s1.to_dictionary(), s2.to_dictionary(), s3.to_dictionary()]
+        jd = Base.to_json_string(dict_s)
+        self.assertTrue(type(jd) is str)
+        dict_s = str(dict_s)
+        dict_s = dict_s.replace("'", '"')
+        self.assertEqual(dict_s, jd)
+
+# ---------- task 17 -------------------------------------------
+
+    def test_20_from_json_string_none(self):
+        """ Test when json_string is passed None"""
+
+        self.assertEqual(Base.from_json_string(None), [])
+        self.assertEqual(Base.from_json_string(""), [])
+
+    def test_21_to_json_string_empty(self):
+        """Test to_json_string with empty"""
+        d = [{}, {}]
+        s = '[{}, {}]'
+        self.assertEqual(Base.from_json_string(s), d)
+
+    def test_22_from_json_string_Rout(self):
+        """Test from_json_string output"""
+        d = [{'x': 1, 'y': 2, 'width': 3, 'id': 4, 'height': 5},
+             {'x': 2, 'y': 4, 'width': 6, 'id': 8, 'height': 10}]
+
+        out = Rectangle.from_json_string(Rectangle.to_json_string(d))
+
+        self.assertEqual(d, out)
+        self.assertTrue(type(out) is list)
+        self.assertEqual(len(out), 2)
+        self.assertTrue(type(out[0]) is dict)
+        self.assertTrue(type(out[1]) is dict)
 
 if __name__ == '__main__':
     unittest.main()

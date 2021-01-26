9ee14fdb399d35177e98063da134c44a5cea7bad
huy75 <huy.nguyen66@gmail.com>
2021-01-26 09:44:25 +0000

    feat add tests Square

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -43,3 +43,190 @@ class TestSquare(unittest.TestCase):
         """Tests Square class type."""
         self.assertEqual(str(Square),
                         "<class 'models.square.Square'>")
+
+    def test_4_id(self):
+        """Prints out the id"""
+        s1 = Square(1, 2)
+        self.assertEqual(s1.id, 1)
+
+        s2 = Square(1, 4)
+        self.assertEqual(s2.id, 2)
+
+        s3 = Square(1, 2, 0, 12)
+        self.assertEqual(s3.id, 12)
+        self.assertTrue(type(s3), Square)
+
+    def test_5_no_args(self):
+        """Tests no arguments."""
+        with self.assertRaises(TypeError) as e:
+            s = Square()
+        s = "__init__() missing 1 required positional argument: 'size'"
+        self.assertEqual(str(e.exception), s)
+
+    def test_6_many_args(self):
+        """Tests many arguments."""
+        with self.assertRaises(TypeError) as e:
+            s = Square(1, 2, 3, 4, 5)
+        s = "__init__() takes from 2 to 5 positional arguments but 6 were \
+given"
+        self.assertEqual(str(e.exception), s)
+
+    def test_7_size(self):
+        """ Test correct size being set"""
+
+        s1 = Square(2, 2)
+        s2 = Square(98, 2)
+
+        self.assertEqual(s1.size, 2)
+        self.assertEqual(s2.size, 98)
+
+    def test_8_type_int(self):
+        """Tests type integer."""
+        r = Square(10)
+        d = {'_Rectangle__height': 10, '_Rectangle__width': 10,
+            '_Rectangle__x': 0, '_Rectangle__y': 0, 'id': 1}
+        self.assertDictEqual(r.__dict__, d)
+
+        with self.assertRaises(TypeError) as e:
+            r = Square("1")
+        s = "width must be an integer"
+        self.assertEqual(str(e.exception), s)
+
+        with self.assertRaises(TypeError) as e:
+            r = Square(1, "2")
+        s = "x must be an integer"
+        self.assertEqual(str(e.exception), s)
+
+        with self.assertRaises(TypeError) as e:
+            r = Square(1, 2, "3")
+        s = "y must be an integer"
+        self.assertEqual(str(e.exception), s)
+
+        with self.assertRaises(ValueError) as e:
+            r = Square(-1)
+        s = "width must be > 0"
+        self.assertEqual(str(e.exception), s)
+
+        with self.assertRaises(ValueError) as e:
+            r = Square(1, -2)
+        s = "x must be >= 0"
+        self.assertEqual(str(e.exception), s)
+
+        with self.assertRaises(ValueError) as e:
+            r = Square(1, 2, -3)
+        s = "y must be >= 0"
+        self.assertEqual(str(e.exception), s)
+
+        with self.assertRaises(ValueError) as e:
+            r = Square(0)
+        s = "width must be > 0"
+        self.assertEqual(str(e.exception), s)
+
+    def test_9_instantiation(self):
+        """Tests positional instantiation."""
+        r = Square(5, 10, 15)
+        d = {'_Rectangle__height': 5, '_Rectangle__width': 5,
+            '_Rectangle__x': 10, '_Rectangle__y': 15, 'id': 1}
+        self.assertEqual(r.__dict__, d)
+
+        r = Square(5, 10, 15, 20)
+        d = {'_Rectangle__height': 5, '_Rectangle__width': 5,
+            '_Rectangle__x': 10, '_Rectangle__y': 15, 'id': 20}
+        self.assertEqual(r.__dict__, d)
+
+    def test_10_keyword(self):
+        """Tests positional instantiation."""
+        r = Square(100, id=421, y=99, x=101)
+        d = {'_Rectangle__height': 100, '_Rectangle__width': 100,
+            '_Rectangle__x': 101, '_Rectangle__y': 99, 'id': 421}
+        self.assertEqual(r.__dict__, d)
+
+    def test_11_id_inherited(self):
+        """Tests if id is inherited from Base."""
+        Base._Base__nb_objects = 98
+        r = Square(2)
+        self.assertEqual(r.id, 99)
+
+    def test_12_properties(self):
+        """Tests property getters/setters."""
+        r = Square(5, 9)
+        r.size = 98
+        r.x = 102
+        r.y = 103
+        d = {'_Rectangle__height': 98, '_Rectangle__width': 98,
+            '_Rectangle__x': 102, '_Rectangle__y': 103, 'id': 1}
+        self.assertEqual(r.__dict__, d)
+        self.assertEqual(r.size, 98)
+        self.assertEqual(r.x, 102)
+        self.assertEqual(r.y, 103)
+
+# ---------- task 3 ---------------------------------------------
+
+    def invalid_types(self):
+        """Returns tuple of types for validation."""
+        t = (3.14, -1.1, float('Nan'), float('-inf'), True, "hello", (2,),
+             [4], {5}, {6: 7}, None)
+        return t
+
+    def test_13_validate_type(self):
+        """Tests property validation."""
+        r = Square(1)
+        attributes = ["x", "y"]
+        for each in attributes:
+            s = "{} must be an integer".format(each)
+            for invalid_type in self.invalid_types():
+                with self.assertRaises(TypeError) as e:
+                    setattr(r, each, invalid_type)
+                self.assertEqual(str(e.exception), s)
+        s = "width must be an integer"
+        for invalid_type in self.invalid_types():
+            with self.assertRaises(TypeError) as e:
+                setattr(r, "width", invalid_type)
+            self.assertEqual(str(e.exception), s)
+
+    def test_14_validate_value_negative_gt(self):
+        """Tests property validation."""
+        r = Square(1, 2)
+        attributes = ["size"]
+        for each in attributes:
+            s = "width must be > 0".format(each)
+            with self.assertRaises(ValueError) as e:
+                setattr(r, each, -33)
+            self.assertEqual(str(e.exception), s)
+
+    def test_15_validate_value_negative_ge(self):
+        """Tests property validation."""
+        r = Square(1, 2)
+        attributes = ["x", "y"]
+        for each in attributes:
+            s = "{} must be >= 0".format(each)
+            with self.assertRaises(ValueError) as e:
+                setattr(r, each, -33)
+            self.assertEqual(str(e.exception), s)
+
+    def test_16_validate_value_zero(self):
+        """Tests property validation."""
+        r = Square(1, 2)
+        attributes = ["size"]
+        for each in attributes:
+            s = "width must be > 0".format(each)
+            with self.assertRaises(ValueError) as e:
+                setattr(r, each, 0)
+            self.assertEqual(str(e.exception), s)
+
+    def test_17_property(self):
+        """Tests property setting/getting."""
+        r = Square(1, 2)
+        attributes = ["x", "y", "width", "height"]
+        for each in attributes:
+            n = 33
+            setattr(r, each, n)
+            self.assertEqual(getattr(r, each), n)
+
+    def test_18_property_range_zero(self):
+        """Tests property setting/getting."""
+        r = Square(1, 2)
+        r.x = 0
+        r.y = 0
+        self.assertEqual(r.x, 0)
+        self.assertEqual(r.y, 0)

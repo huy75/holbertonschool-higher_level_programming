a79f6f348b3d9be957995d88c146978b977c549f
huy75 <huy.nguyen66@gmail.com>
2021-03-03 09:08:05 +0000

    fix add ASC

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
 FROM `tv_shows`
 JOIN `tv_show_genres` ON `tv_show_genres`.`show_id` = `tv_shows`.` id`
-ORDER BY `title`, `genre_id`;
+ORDER BY `title`, `genre_id` ASC;

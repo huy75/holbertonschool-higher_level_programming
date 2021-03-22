c4354b39d92d5e72c6d89e514ba379cb741ca53d
huy75 <huy.nguyen66@gmail.com>
2021-03-22 16:02:11 +0000

    feat second biggest handle duplicate

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// searches the second biggest integer in the list of arguments.
+
+const args = process.argv;
+if (process.argv.length === 2 || process.argv.length === 3) {
+  console.log(0);
+} else {
+  let idx;
+  for (idx = 0; idx < process.argv.length; idx++) {
+    args[idx] = parseInt(process.argv[idx]);
+  }
+  // eliminate duplicates
+  const newA = Array.from([...new Set(args)]);
+  console.log(newA.slice(2).sort(function (a, b) { return b - a; })[1]);
+}

de37b0dbfe237d13378b1d15fa8f2298e934c030
huy75 <huy.nguyen66@gmail.com>
2021-03-22 13:17:04 +0000

    fix move closing curly brace

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -6,4 +6,5 @@ but by using an array of string and a loop
 const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (const line of lines) {
-  console.log(line);}
+  console.log(line);
+}

2743beb455c5f73abab2b804c8683e9dc1359a61
huy75 <huy.nguyen66@gmail.com>
2021-03-22 11:09:42 +0000

    feat tasks 0-2

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// Prints out a message to the log
+const myVar = 'Javascript is amazing';
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Prints out a message to the log
+const c = 'C is fun';
+const python = 'Python is cool';
+const js = 'Javascript is amazing';
+console.log(c + '\n' + python + '\n' + js);

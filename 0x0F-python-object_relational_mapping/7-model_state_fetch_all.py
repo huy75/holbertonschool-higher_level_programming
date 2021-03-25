815c300dad4dcc6c0e06a495a574da026a4a059a
huy75 <huy.nguyen66@gmail.com>
2021-03-25 16:33:46 +0000

    feat add comments

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,13 +6,23 @@ if __name__ == "__main__":
     from sqlalchemy.orm import sessionmaker
     from model_state import State, Base
 
+    # Base is an instance of the declarative base class
     if len(argv) == 4:
+        # creates a SQLAlchemy Engine
         engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+        """
+        CREATE TABLE statements to the database for all tables
+        that don't yet exist
+        """
         Base.metadata.create_all(engine)
 
+        # creates a SQLAlchemy ORM session factory bound to this engine
         Session = sessionmaker(bind=engine)
+        # creates a session
         session = Session()
+
         query = session.query(State).order_by(State.id).all()
 
         for row in query:
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,11 +4,15 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+# Base is an instance of the declarative base class
 Base = declarative_base()
 
-
+"""
+Creates a class that includes directives to describe
+the actual database table it will be mapped to.
+"""
 class State(Base):
-    """inherits from Base"""
+    """the class to which the table 'states' is mapped to"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False,
                 unique=True, autoincrement=True)

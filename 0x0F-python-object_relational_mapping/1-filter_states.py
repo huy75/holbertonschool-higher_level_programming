05a3e1dbad687e379ee3b9795109c69a03880f3a
huy75 <huy.nguyen66@gmail.com>
2021-03-25 11:08:07 +0000

    feat states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# lists all states with a name starting with N (upper N)
+# from the database hbtn_0e_0_usa
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    if len(argv) == 4:
+        db = MySQLdb.connect(host="localhost", user=argv[1],
+                             passwd=argv[2], db=argv[3], port=3306,
+                             charset="utf8")
+        query = "SELECT id, name FROM states WHERE name LIKE BINARY 'N%'\
+        ORDER BY id ASC;"
+        with db.cursor() as cur:
+            cur.execute(query)
+            for row in cur:
+                print(row)
+        db.close()
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# displays all values in the states table of hbtn_0e_0_usa
+# where name matches the argument.
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    if len(argv) == 5:
+        db = MySQLdb.connect(host="localhost", user=argv[1],
+                             passwd=argv[2], db=argv[3], port=3306,
+                             charset="utf8")
+        query = """ SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY
+        states.id ASC"""
+        query = query.format(argv[4])
+
+        with db.cursor() as cur:
+            cur.execute(query)
+            for row in cur:
+                print(row)
+        db.close()

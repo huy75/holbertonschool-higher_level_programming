f7e933fc10d425690452da7d2dddf0422e9bb4ae
huy75 <huy.nguyen66@gmail.com>
2021-03-27 13:20:18 +0000

    fix use HB's template

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-# displays all cities of hbtn_0e_0_usa
+""" displays all cities of hbtn_0e_0_usa """
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    if len(argv) == 4:
-        db = MySQLdb.connect(host="localhost", user=argv[1],
-                             passwd=argv[2], db=argv[3], port=3306,
-                             charset="utf8")
-        query = "SELECT cities.id, cities.name, states.name FROM cities " +\
+    usrName = argv[1]
+    pssWd = argv[2]
+    dbName = argv[3]
+
+    conn = MySQLdb.connect(host="localhost", user=usrName,
+                           passwd=pssWd, db=dbName, port=3306,
+                           charset="utf8")
+
+    query = "SELECT cities.id, cities.name, states.name FROM cities " +\
                 "LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;"
 
-        with db.cursor() as cur:
-            cur.execute(query)
-            for row in cur:
-                print(row)
-        db.close()
+    cur = conn.cursor()
+    cur.execute(query)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-# takes in the name of a state as an argument and lists all cities
-# of that state, using the database hbtn_0e_4_usa
+"""
+takes in the name of a state as an argument and lists all cities
+of that state, using the database hbtn_0e_4_usa
+"""
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    if len(argv) == 5:
-        db = MySQLdb.connect(host="localhost", user=argv[1],
-                             passwd=argv[2], db=argv[3], port=3306,
-                             charset="utf8")
-        query = "SELECT cities.name FROM cities " +\
-                "LEFT JOIN states ON states.id = cities.state_id " +\
-                "WHERE states.name = %s ORDER BY cities.id;"
+    usrName = argv[1]
+    pssWd = argv[2]
+    dbName = argv[3]
+    nameSrch = argv[4]
 
-        with db.cursor() as cur:
-            cur.execute(query, (argv[4], ))
-            cities = cur.fetchall()
-            cities = [city[0] for city in cities]
-            print(", ".join(cities))
-        db.close()
+    conn = MySQLdb.connect(host="localhost", user=usrName,
+                           passwd=pssWd, db=dbName, port=3306,
+                           charset="utf8")
+    query = "SELECT cities.name FROM cities " +\
+            "LEFT JOIN states ON states.id = cities.state_id " +\
+            "WHERE states.name = %s ORDER BY cities.id;"
+
+    cur = conn.cursor()
+    cur.execute(query, {'name': nameSrch})
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()

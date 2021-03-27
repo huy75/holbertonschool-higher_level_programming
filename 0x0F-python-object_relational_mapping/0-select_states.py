0931997c502a9ab245f8bfa9a4d02ef840546541
huy75 <huy.nguyen66@gmail.com>
2021-03-27 12:50:46 +0000

    fix remove nb of args test

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,18 +4,19 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    if len(argv) == 4:
-        usrName = argv[1]
-        pssWd = argv[2]
-        dbName = argv[3]
-        conn = MySQLdb.connect(host="localhost", user=usrName,
-                             passwd=pssWd, db=dbName, port=3306,
-                             charset="utf8")
-        query = "SELECT id, name FROM states ORDER BY states.id ASC;"
-        cur = conn.cursor()
-        cur.execute(query)
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-        cur.close()
-        conn.close()
+    usrName = argv[1]
+    pssWd = argv[2]
+    dbName = argv[3]
+
+    conn = MySQLdb.connect(host="localhost", user=usrName,
+                           passwd=pssWd, db=dbName, port=3306,
+                           charset="utf8")
+
+    query = "SELECT id, name FROM states ORDER BY states.id ASC;"
+    cur = conn.cursor()
+    cur.execute(query)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()

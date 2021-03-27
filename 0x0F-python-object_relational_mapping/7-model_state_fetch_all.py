823e1d678be9bc28029315dc07f1624b4030cb1a
huy75 <huy.nguyen66@gmail.com>
2021-03-27 13:48:45 +0000

    fix comments

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-# lists all State objects from the database hbtn_0e_6_usa
+""" lists all State objects from the database hbtn_0e_6_usa """
 if __name__ == "__main__":
     from sys import argv
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
     from model_state import State, Base
 
-    # Base is an instance of the declarative base class
+    """ Base is an instance of the declarative base class """
     if len(argv) == 4:
-        # creates a SQLAlchemy Engine
+        """ creates a SQLAlchemy Engine """
         engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                                .format(argv[1], argv[2],
                                        argv[3]), pool_pre_ping=True)

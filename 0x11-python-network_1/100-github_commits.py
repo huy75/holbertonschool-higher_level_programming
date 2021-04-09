85e39c6a59e9bdc68e873e442fe16b5023984570
huy75 <huy.nguyen66@gmail.com>
2021-04-09 20:26:09 +0000

    fix import

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,19 +3,14 @@
 Users github api to list out 10 most recent commits
 from specified repo and user
 """
-import requests
-from sys import argv
-
 if __name__ == "__main__":
-    repo = argv[1]
-    owner = argv[2]
-    url = "http://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    import requests
+    import sys
 
-    response = requests.get(url).json()
-    for r in response[:10]:
-        print("{}: {}".format(r.get('sha'),
-                              r.get('commit').get('author').get('name')))
-    """
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    u = "http://api.github.com/repos/{}/{}/commits".format(owner, repo)
+
     response = requests.get(url=u, params={'per_page': 10})
     if response.status_code != 200:
         print("None")
@@ -25,4 +20,3 @@ if __name__ == "__main__":
             print("{}: {}".format(
                 each.get("sha"),
                 each.get("commit").get("author").get("name")))
-    """

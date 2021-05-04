8136e3e7ccb462f17caf4782b842840509e7d40b
huy75 <huy.nguyen66@gmail.com>
2021-05-04 15:08:02 +0000

    fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -8,11 +8,15 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request.get(url, (error, response, data) => {
   if (error) console.log(error);
   else {
-    const characters = JSON.parse(data).characters;
+    const film = JSON.parse(data);
+    const characters = film.characters;
     for (const character of characters) {
       request(character, (error, response, data) => {
         if (error) console.log(error);
-        else console.log(JSON.parse(data).name);
+        else {
+          const chars = JSON.parse(data);
+          console.log(chars.name);
+        }
       });
     }
   }

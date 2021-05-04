d58c81ece743f2d0fe761a548f0c0f7580d737a8
huy75 <huy.nguyen66@gmail.com>
2021-05-04 09:20:21 +0000

    feat task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// prints the title of a Star Wars movie
+// where the episode number matches a given integer
+
+const request = require('request');
+let url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request.get(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
+});

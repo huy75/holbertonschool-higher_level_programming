f9749eda87480c526ca028c640a35376bacd118a
huy75 <huy.nguyen66@gmail.com>
2021-05-04 09:21:29 +0000

    fix semistandard compliant

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@
 // where the episode number matches a given integer
 
 const request = require('request');
-let url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request.get(url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);

f951acd8451f7a0ce4561ab823b3354924aa33ad
huy75 <huy.nguyen66@gmail.com>
2021-05-05 10:31:37 +0000

    fix recursive

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,7 +7,7 @@
 
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-
+/*
 request.get(url, (error, response, data) => {
   if (error) console.log(error);
   else {
@@ -26,3 +26,25 @@ request.get(url, (error, response, data) => {
     }
   }
 });
+*/
+
+request.get(url, (error, response, data) => {
+  if (error) console.log(error);
+  else {
+    const character = JSON.parse(data).characters;
+    printcur(character, 0);
+  }
+});
+
+function printcur (arr, idx) {
+  if (idx >= arr.length) {
+    return;
+  }
+  request.get(arr[idx], (e, r, b) => {
+    if (e) console.log(e);
+    else {
+      console.log(JSON.parse(b).name);
+    }
+    return printcur(arr, idx + 1);
+  });
+}

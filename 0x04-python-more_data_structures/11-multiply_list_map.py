df38c97884f6c415bc8cba50cc38501a9d4a5645
huy75 <huy.nguyen@holbertonschool.com>
2021-11-10 09:18:43 +0000

    add some comments

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+# map(func, seq) applies the function func to all the elements of the sequence seq
+# map() returns an iterator
 def multiply_list_map(my_list=[], number=0):
     return list(map(lambda x: x * number, my_list))

e1d3b35bf2783dc06e7f908e3fd028ad1a4bada8
huy75 <huy.nguyen@holbertonschool.com>
2021-11-15 10:48:27 +0000

    add log parser

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+""" script that reads stdin line by line and computes metrics"""
+import sys
+
+
+if __name__ == "__main__":
+    """ Code to retrieve and display datas"""
+    size = 0
+    statdict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+    def displaystat(size, statdict):
+        """ function to print the stats"""
+        print("File size: {}".format(size))
+        for key, value in sorted(statdict.items()):
+            if value != 0:
+                print("{}: {}".format(key, value))
+
+    nbline = 1
+    try:
+        """ Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size> """
+        for line in sys.stdin:
+            try:
+                linetok = line.split(" ")
+                stat = int(linetok[-2])
+                if stat in statdict:
+                    statdict[stat] += 1
+                    size += int(linetok[-1])
+            except:
+                pass
+
+            if nbline % 10 == 0:
+                displaystat(size, statdict)
+            nbline += 1
+
+    except KeyboardInterrupt:
+        displaystat(size, statdict)
+        raise
+    displaystat(size, statdict)
+    
--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -7,6 +7,6 @@ Defines a Python class-to-JSON function.
 
 def class_to_json(obj):
     """
-    Return the dictionary represntation of a simple data structure.
+    Return the dictionary representation of a simple data structure.
     """
     return (obj.__dict__)
